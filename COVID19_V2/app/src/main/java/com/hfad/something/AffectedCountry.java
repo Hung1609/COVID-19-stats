56e3304ebe08e3c206f6ffc0cd1485c3836b4513
Ky Duyen <hoangkyduyenn04@gmail.com>
2024-09-16 13:27:24 +0700

    add glide

--- COVID19_V2/app/src/main/java/com/hfad/something/AffectedCountry.java
+++ COVID19_V2/app/src/main/java/com/hfad/something/AffectedCountry.java
@@ -10,10 +10,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AffectedCountry extends AppCompatActivity {
     EditText edtSearch;
     ListView listView;
 
+    public static List<CountryModel> countryModelList = new ArrayList<>();
+    CountryModel countryModel;
+    MyCustomAdapter myCustomAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,9 @@ public class AffectedCountry extends AppCompatActivity {
         setContentView(R.layout.activity_affected_country);
         edtSearch = findViewById(R.id.edtSearch);
         listView = findViewById(R.id.listView);
-
-        };
+        fetchData();
+    };
+    private void fetchData(){
+    String url ="//api.covid19api.com/summary"; //chua co API
+    }
     }
